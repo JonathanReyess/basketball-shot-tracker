2615760453386cade0b46bbd33870970fbdb7b60
Jonathan Reyes <jer88@duke.edu>
2024-01-18 02:30:58 -0500

    revamped

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-This is a basic website that can be used to calculate the field goal percentage of a player during the game of basketball. 
-
-The **'bucket'** refers to a shot that has been made whereas the **'brick'** corresponds to a miss.
-
-Field goal percentage is calculated using the formula:
-
-_# of shots made / # of shots attempted_
+This is a basic website that will allow you to calculate your field goal %, the ratio of field goals made to field goals attempted, in percent form. This website can be used to determine how well you are shooting the ball.
--- basketball.js
+++ basketball.js
@@ -2,10 +2,10 @@
 //defining our variables
 var buttons = document.getElementById("bucket");
 buttons.innerHTML =
-  '<img src="https://cdn.glitch.me/d1fddca7-50ba-4596-aae1-4598c4cb77da%2Fbrick2.png?v=1638239011894" height="100px" width="100px"" />';
+  '<img src="https://cdn.glitch.me/d1fddca7-50ba-4596-aae1-4598c4cb77da%2Fbucket.png?v=1638238154930" height="100px" width="100px"" />';
 var buttons = document.getElementById("brick");
 buttons.innerHTML =
-'<img src="https://cdn.glitch.me/d1fddca7-50ba-4596-aae1-4598c4cb77da%2Fbucket.png?v=1638238154930" height="100px" width="100px"" />';
+'<img src="https://cdn.glitch.me/d1fddca7-50ba-4596-aae1-4598c4cb77da%2Fbrick2.png?v=1638239011894" height="100px" width="100px"" />';
 
 
 //function that determines what the brick button does
@@ -31,9 +31,15 @@ function incrementbothValue() {
 
 //function that evaluates the digits and returns the field goal percentage
 function solve() {
-  let x = document.getElementById("bucket").value;
-  let y = document.getElementById("brick").value;
-  document.getElementById("result").value = (x / y) * 100;
+  let x = parseInt(document.getElementById("bucket").value, 10);
+  let y = parseInt(document.getElementById("brick").value, 10);
+
+  if (isNaN(x) || isNaN(y) || y === 0) {
+    document.getElementById("result").value = "Invalid Input";
+  } else {
+    let percentage = Math.round((x / y) * 100);
+    document.getElementById("result").value = percentage + "%";
+  }
 }
 
 //function that clear the display
